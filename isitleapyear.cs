e67ff97f378ef7fc8c92d652f72af6210e7ebac7
martin010919 <70171040+martin010919@users.noreply.github.com>
2021-04-09 12:41:06 +0200

    C# code
    It can calculate, if the date what u put in, a leap year or not

---
+++ isitleapyear.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace szökőéve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("write the date in");
+            string evszam = Console.ReadLine();
+            int a = Convert.ToInt32(evszam);
+            DateTime.IsLeapYear(a);
+            
+            if (true a / 4.00);
+            {
+                Console.WriteLine("leap year!");
+            }
+            if (false a / 4.00);
+            {
+                Console.WriteLine("not leap year");
+            }
+
+            
+            Console.ReadKey();
+        }
+    }
+}
